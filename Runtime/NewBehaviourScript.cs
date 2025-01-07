15699392c93a58c0d3081ea894209fbd49a45b4a
QuangHaiVu <zzquanghai0108zz@gmail.com>
2025-01-07 13:37:42 +0700

    fix: update depedencies

--- Runtime/NewBehaviourScript.cs
+++ Runtime/NewBehaviourScript.cs
@@ -6,16 +6,6 @@ namespace thelegends.Test2
 {
     public class NewBehaviourScript : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
